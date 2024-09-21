c5de33d6540149480a4532f5f7e3e42f55d79c6b
Leila828 <l.habib@esi-sba.dz>
2024-09-21 23:06:44 +0100

    adding pdf downloader from website url

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # This is a sample Python script.
 from flask import Flask, request, jsonify
+
+from data.data_fetcher import fetch_first_download_url, download_gem_report
 from utils.rag import chat_with_rag
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -22,9 +25,22 @@ def query():
     urls = [
         "https://data.worldbank.org/indicator/SI.POV.DDAY?locations=1W-BR",
         "https://data.worldbank.org/indicator/IT.NET.USER.ZS?locations=1W-BR",
+        "https://data.worldbank.org/indicator/SL.UEM.TOTL.ZS?locations=1W-BR",
+
 
     ]
-    pdf_path = "data/GEM.pdf"  # Replace with your actual PDF path
+
+    try:
+        # Step 1: Fetch the first download URL
+        download_url = fetch_first_download_url()
+
+        # Step 2: Download the report using the constructed URL
+        download_gem_report(download_url, output_path="data/gem_report.pdf")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+    pdf_path = "data/gem_report.pdf"
     response = chat_with_rag(urls, query, pdf_path)
 
     return jsonify({"response": response})
